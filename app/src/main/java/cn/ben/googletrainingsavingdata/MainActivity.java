a23fe62f71ce35c58a8ea3e068a3cb822fd575fa
van048 <yybbenyang@gmail.com>
2016-10-12 15:01:34 +0800

    Saving Key-Value Sets

---
+++ app/src/main/java/cn/ben/googletrainingsavingdata/MainActivity.java
@@ -0,0 +1,20 @@
+package cn.ben.googletrainingsavingdata;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, new SharedPreferencesFragment());
+        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/cn/ben/googletrainingsavingdata/SharedPreferencesFragment.java
@@ -0,0 +1,92 @@
+package cn.ben.googletrainingsavingdata;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SharedPreferencesFragment extends Fragment implements View.OnClickListener {
+
+    private SharedPreferences sp1;
+    @SuppressWarnings("unused")
+    SharedPreferences sp2;
+    private EditText inputHighScore;
+
+    public SharedPreferencesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        sp1 = getHandleToSP1();
+        sp2 = getHandleToSP2();
+    }
+
+    private void writeNewHighScoreToSP1(int newHighScore) {
+        if (sp1 == null) sp1 = getHandleToSP1();
+        SharedPreferences.Editor editor = sp1.edit();
+        editor.putInt(getString(R.string.saved_high_score), newHighScore);
+        editor.apply();
+    }
+
+    private long readHighScoreFromSP1() {
+        if (sp1 == null) sp1 = getHandleToSP1();
+        int defaultValue = getResources().getInteger(R.integer.saved_high_score_default);
+        return (long) sp1.getInt(getString(R.string.saved_high_score), defaultValue);
+    }
+
+    private SharedPreferences getHandleToSP2() {
+        return getActivity().getPreferences(Context.MODE_PRIVATE);
+    }
+
+    private SharedPreferences getHandleToSP1() {
+        Context context = getActivity();
+        return context.getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_shared_preferences, container, false);
+
+        Button btnWritePref = (Button) view.findViewById(R.id.btn_write_pref);
+        btnWritePref.setOnClickListener(this);
+        Button btnReadPref = (Button) view.findViewById(R.id.btn_read_pref);
+        btnReadPref.setOnClickListener(this);
+        inputHighScore = (EditText) view.findViewById(R.id.input_high_score);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_write_pref:
+                String scoreStr = inputHighScore.getText().toString();
+                int score;
+                try {
+                    score = Integer.parseInt(scoreStr);
+                    writeNewHighScoreToSP1(score);
+                    Toast.makeText(getContext(), "Modification Applying!", Toast.LENGTH_SHORT).show();
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getContext(), "Wrong input!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.btn_read_pref:
+                Toast.makeText(getContext(), "current high score: " + readHighScoreFromSP1(), Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+    }
+}
